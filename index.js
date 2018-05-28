685f054ad1461e48931c71df11c70426707a6dd2
Dana F <danatania@gmail.com>
2018-05-28 22:40:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(string) {
-  return 'Happy holidays, ${name}!'
+function happyHolidaysTo(name) {
+  console.log('Happy holidays, ${name}!')
 }
